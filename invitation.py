6fb20e5930a3a7487411d3ba221ebeb661521e6f
edna <cao.edna@gmail.com>
2015-11-23 19:46:12 -0800

    arc length calculating

--- invitation.py
+++ invitation.py
@@ -19,15 +19,25 @@ for line in open("customers.json"):
 # longitude_dublin = -6.2592576
 
 EARTH_RADIUS_KM = 6367
+DUBLIN_LATITUDE = 53.3381985
+DUBLIN_LONGITUDE = -6.2592576
+
+latitude_2 = 52.986375
+longitude_2 = -6.043701
 
 def spherical_law_of_cosines():
 	central_angle = math.acos(
-		(math.sin(latitude_dublin) * 
+		(math.sin(DUBLIN_LATITUDE) * 
 		math.sin(latitude_2)) + 
-		(math.cos(latitude_dublin) * 
+		(math.cos(DUBLIN_LATITUDE) * 
 		math.cos(latitude_2)) * 
-		(math.cos(longitude_2 - longitude_dublin))
+		(math.cos(longitude_2 - DUBLIN_LONGITUDE))
 	)
 
-def great_circle_distance():
-	return
+	arc_length = EARTH_RADIUS_KM * math.radians(central_angle)
+	print arc_length
+
+
+spherical_law_of_cosines()
+
+
