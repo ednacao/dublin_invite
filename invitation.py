8eca150a05ede21b48760c747b668271729ce1db
edna <cao.edna@gmail.com>
2016-01-20 15:54:59 -0800

    Set arc length function

--- invitation.py
+++ invitation.py
@@ -5,38 +5,39 @@ Output the names and user ids of matching customers (within 100 km), sorted by u
 import json
 import math
 
-def calculate_arc_length():
+def calculate_arc_length(latitude,longitude):
 	# Constants
 	EARTH_RADIUS_KM = 6367
 	DUBLIN_LATITUDE = 53.3381985
 	DUBLIN_LONGITUDE = -6.2592576
 
+	# Spherical Law of Cosines
+	central_angle = math.acos(
+					(math.sin(DUBLIN_LATITUDE) * 
+					math.sin(latitude)) + 
+					(math.cos(DUBLIN_LATITUDE) * 
+					math.cos(latitude)) * 
+					(math.cos(longitude - DUBLIN_LONGITUDE))
+					)
+
+	return EARTH_RADIUS_KM * math.radians(central_angle)
+
+
+def parse_file():
 	near_dublin_dict = {}
 	# Load in file and parse values
 	for line in open("customers.json"):
 		line = line.strip() # <type str>
 		parsed_json = json.loads(line) # <type dict>
 
-		latitude = float(parsed_json['latitude'])
+		latitude_foo = float(parsed_json['latitude'])
 		user_id = int(parsed_json['user_id'])
 		name = parsed_json['name']
-		longitude = float(parsed_json['longitude'])
+		longitude_foo = float(parsed_json['longitude'])
 
-#function 2
-		# Spherical Law of Cosines
-		central_angle = math.acos(
-						(math.sin(DUBLIN_LATITUDE) * 
-						math.sin(latitude)) + 
-						(math.cos(DUBLIN_LATITUDE) * 
-						math.cos(latitude)) * 
-						(math.cos(longitude - DUBLIN_LONGITUDE))
-						)
-
-		arc_length = EARTH_RADIUS_KM * math.radians(central_angle)
-
+		arc_length = calculate_arc_length(latitude_foo, longitude_foo)
 
 		if arc_length < 100:
-			# print "ID:",user_id,name,"Arc length:",arc_length
 			near_dublin_dict[user_id] = name
 
 
@@ -45,6 +46,5 @@ def calculate_arc_length():
 		print "ID:", item, "Name:", near_dublin_dict[item]
 
 
-
-calculate_arc_length()
+parse_file()
 
